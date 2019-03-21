9346122507e236e47059f67a0aa4b80fdf640bd6
hdenes <helgadenes@gmail.com>
2019-03-21 14:46:46 +0100

    Update make_mosaic_image.py
    Updated mosaicing script. Now the script takes the obs_id as a parameter.

--- make_mosaic_image.py
+++ make_mosaic_image.py
@@ -1,27 +1,77 @@
 #!/usr/bin/env python
 
+
+"""
+This script creates a mosaic file from apertif continuum images.
+
+- This script takes the obs_id as an argument, 
+- looks for continuum images from the pipeline (fits format), 
+- copies the images into a temporary directory, 
+- creates a mosaic image in /data/apertif/obs_id/mosaic/
+- and deletes the temporary directory. 
+
+Parameter
+    obs_id : int
+        Observation number which should be assessed
+
+Example:
+    python make_mosaic_image.py obs_id
+
+"""
+
 import os
 import sys
 import apercal.libs.lib as lib
 import subprocess  
+import socket
+import argparse
 
-# create a mosaic file from apertif continuum images 
-# this script assumes the output of the automated pipeline 
 #------------------------------------------
+#data_dir = '190311152'
 
-data_dir = '190311152'
+#myusername = os.environ['USER']
 
-myusername = os.environ['USER']
-#print('user:', myusername)
+# Check what host the user is on
+#-------------------------------------
+
+host_name = socket.gethostname()
+
+if host_name != "happili-01":
+	print("WARNING: You are not working on happili-01.")
+	print("WARNING: The script will not process all beams")
+	print("Please switch to happili-01")
+
+
+# Create and parse argument list
+#---------------------------------------
+parser = argparse.ArgumentParser(
+	description='Create mosaic image from pipeline continuum images')
+
+# main argument: Observation number
+parser.add_argument("obs_id", type=str,
+					help='Observation Number / Scan Number / TASK-ID')
+
+args = parser.parse_args()
+
+# Basic parameters
+#-----------------------------------------
+
+# get the observation number
+data_dir = args.obs_id
+print(data_dir)
+
 
 #-------------------------------------------
 # search for continuum files in standard pipeline directories
 # and copy the files into a temporary directory
 # if there is no fits file in the continuum folder, there will be an error message, but the script will continue running 
 
-os.system('mkdir /data/apertif/'+str(data_dir)+'/mosaic/') 
-os.system('mkdir /data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/') 
+if not os.path.exists('/data/apertif/'+str(data_dir)+'/mosaic/'):
+	os.mkdir('/data/apertif/'+str(data_dir)+'/mosaic/')
 
+if not os.path.exists('/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/'):
+	os.mkdir('/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/')
+         
 
 beams_1 = os.listdir('/data/apertif/'+str(data_dir))
 beams_2 = os.listdir('/data2/apertif/'+str(data_dir))
@@ -33,7 +83,7 @@ beams_4 = os.listdir('/data4/apertif/'+str(data_dir))
 def copy_data(beams, n, data):
 	for i in beams:
 		if os.path.isdir('/'+str(data)+'/apertif/'+str(data_dir)+'/'+str(i)) == True and n in i:
-			os.system('cp -r /'+str(data)+'/apertif/'+str(data_dir)+'/'+str(i)+'/continuum/image_mf_*.fits /data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/image_mf_'+str(i)+'.fits') 
+			os.system('cp -r /'+str(data)+'/apertif/'+str(data_dir)+'/'+str(i)+'/continuum/image_mf_*.fits /data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/image_mf_'+str(i)+'.fits') 
 
 
 copy_data(beams_1, '0', 'data')
@@ -45,13 +95,13 @@ copy_data(beams_4, '3', 'data4')
 # convert fits continuum images into miriad files
 # (this may not be needed)
 
-items = os.listdir('/data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/')
+items = os.listdir('/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/')
 
 def convert_fits():
 	for i in range(len(items)):
 		fits = lib.miriad('fits')
-		fits.in_ = '/data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/'+str(items[i])
-		fits.out= '/data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/'+str(items[i][:-5])+'.mir'
+		fits.in_ = '/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/'+str(items[i])
+		fits.out= '/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/'+str(items[i][:-5])+'.mir'
 		fits.op = 'xyin'
 		fits.go()
 
@@ -60,24 +110,26 @@ convert_fits()
 #-----------------------------------
 # create mosaic image with linmos
 
+print('/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/image_mf_*.mir')
+
 linmos = lib.miriad('linmos')
-linmos.in_ = '/data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/image_mf_*.mir'
-linmos.out= '/data/apertif/'+str(data_dir)+'/mosaic/'+str(data_dir)+'_mosaick_image'
+linmos.in_ = '/data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/image_mf_*.mir'
+linmos.out = '/data/apertif/'+str(data_dir)+'/mosaic/'+str(data_dir)+'_mosaic_image'
 linmos.go()
 
 #-----------------------------------
 # convert mosaic miriad image into fits file
 
 fits = lib.miriad('fits')
-fits.in_ = '/data/apertif/'+str(data_dir)+'/mosaic/'+str(data_dir)+'_mosaick_image'
+fits.in_ = '/data/apertif/'+str(data_dir)+'/mosaic/'+str(data_dir)+'_mosaic_image'
 fits.op = 'xyout'
-fits.out = '/data/apertif/'+str(data_dir)+'/mosaic/'+str(data_dir)+'_mosaick_image.fits'
+fits.out = '/data/apertif/'+str(data_dir)+'/mosaic/'+str(data_dir)+'_mosaic_image.fits'
 fits.go()
 
 #-------------------------------------
 #clen up temporary files
 
-os.system('rm -rf /data/apertif/'+str(data_dir)+'/mosaic/continuum_tmp/') 
+os.system('rm -rf /data/apertif/'+str(data_dir)+'/mosaic/cont_tmp/') 
 
 
 print("DONE") 
