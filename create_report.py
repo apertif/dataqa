5a434f5f3d37f4d4d8f49c7433e2e9fd4d61fe09
Robert Schulz <schulz@astron.nl>
2019-03-21 11:46:58 +0100

    fixed logging and qa dir for main script

--- create_report.py
+++ create_report.py
@@ -11,8 +11,10 @@ import logging
 import glob
 import time
 import argparse
-from report import html_report as hp
-from report import html_report_dir as hpd
+from apercal.libs import lib
+from dataqa.report import html_report as hp
+from dataqa.report import html_report_dir as hpd
+from datqa.scandata import get_default_imagepath
 
 if __name__ == "__main__":
 
@@ -27,22 +29,46 @@ if __name__ == "__main__":
     parser.add_argument("obs_id", type=int,
                         help='Observation Number')
 
-    parser.add_argument("--qa_dir", type=str, default='',
-                        help='Observation Number')
+    parser.add_argument("-p", "--path", type=str,
+                        help='Path to QA output')
 
     args = parser.parse_args()
 
     obs_id = args.obs_id
-    qa_dir = args.qa_dir
+    qa_dir = args.path
 
-    logging.basicConfig(filename='{0:s}/create_report.log'.format(qa_dir), level=logging.DEBUG,
-                        format='%(asctime)s - %(levelname)s: %(message)s')
+    # directory where the output will be of pybdsf will be stored
+    if qa_dir is None:
+        qa_dir = get_default_imagepath(obs_id)
+
+        # check that path exists
+        if not os.path.exists(qa_dir):
+            print(
+                "Directory {0:s} does not exist and will be created".format(qa_dir))
+            os.mkdir(qa_dir)
+
+    # check the mode to run the validation
+    qa_report_dir = "{0:s}report".format(
+        qa_dir)
+
+    # check that this directory exists (just in case)
+    if not os.path.exists(qa_report_dir):
+        print("Directory {0:s} does not exist and will be created".format(
+            qa_report_dir))
+        os.mkdir(qa_report_dir)
+
+    lib.setup_logger(
+        'debug', logfile='{0:s}/create_report.log'.format(qa_report_dir))
+    logger = logging.getLogger(__name__)
+
+    # logging.basicConfig(filename='{0:s}/create_report.log'.format(qa_dir), level=logging.DEBUG,
+    #                     format='%(asctime)s - %(levelname)s: %(message)s')
 
     # the subpages to be created
     subpages = ['prepare', 'crosscal',
                 'continuum', 'selfcal', 'line', 'mosaic', 'apercal_log']
 
-    print("Create report directory structure")
+    logger.info("#### Create report directory structure")
 
     # copy the js and css files
     js_file_name = "report/report_fct.js"
@@ -50,21 +76,22 @@ if __name__ == "__main__":
 
     # Check that qa_dir and the other directories exists
     if not os.path.exists(qa_dir):
-        logging.error("Directory {0:s} does not exists. Abort".format(qa_dir))
+        logger.error("Directory {0:s} does not exists. Abort".format(qa_dir))
         sys.exit(-1)
     else:
         # Create directory structure for the report
         create_dir_stat = hpd.create_report_dirs(
             obs_id, qa_dir, subpages, css_file=css_file_name, js_file=js_file_name)
 
-    print("Creating report")
+    logger.info("### Creating report")
 
     create_main_html_stat = hp.create_main_html(
         qa_dir, obs_id, subpages, css_file=css_file_name, js_file=js_file_name)
 
     if create_main_html_stat == 1:
-        print("Creating report succesfull")
+        logger.info("Creating report succesfull")
     else:
-        print("ERROR: Creating report failed")
+        logger.error("Creating report failed")
 
-    print("Creating report. Done ({0:.0f}s)".format(time.time()-start_time))
+    logger.info("Report. Done ({0:.0f}s)".format(
+        time.time()-start_time))
