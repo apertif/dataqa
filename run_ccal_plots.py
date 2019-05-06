b38f7a11bd562575c5cd554b3a2a8dff6c08c94e
Robert Schulz <schulz@astron.nl>
2019-05-06 17:31:35 +0200

    Added main function

--- run_ccal_plots.py
+++ run_ccal_plots.py
@@ -13,43 +13,45 @@ import logging
 import os
 from apercal.libs import lib
 
-start = timer()
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser(description='Generate crosscal QA plots')
+    start = timer()
 
-# 1st argument: File name
-parser.add_argument("scan", help='Scan of target field')
-parser.add_argument("fluxcal", help='Fluxcal name')
-parser.add_argument("polcal", help='Polcal name')
+    parser = argparse.ArgumentParser(description='Generate crosscal QA plots')
 
-parser.add_argument('-p', '--path', default=None,
-                    help='Destination for images')
+    # 1st argument: File name
+    parser.add_argument("scan", help='Scan of target field')
+    parser.add_argument("fluxcal", help='Fluxcal name')
+    parser.add_argument("polcal", help='Polcal name')
 
-args = parser.parse_args()
+    parser.add_argument('-p', '--path', default=None,
+                        help='Destination for images')
 
-# If no path is given change to default QA path
-if args.path is None:
-    output_path = get_default_imagepath(args.scan)
+    args = parser.parse_args()
 
-    # check that crosscal qa directory exists
-    output_path = "{0:s}crosscal/".format(output_path)
+    # If no path is given change to default QA path
+    if args.path is None:
+        output_path = get_default_imagepath(args.scan)
 
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-else:
-    output_path = args.path
+        # check that crosscal qa directory exists
+        output_path = "{0:s}crosscal/".format(output_path)
 
-# Create logging file
-lib.setup_logger(
-    'debug', logfile='{0:s}run_ccal_plots.log'.format(output_path))
-logger = logging.getLogger(__name__)
-# logging.basicConfig(level=logging.DEBUG)
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+    else:
+        output_path = args.path
 
-# Create crosscal plots
-crosscal_plots.make_all_ccal_plots(
-    args.scan, args.fluxcal, args.polcal, output_path=output_path)
+    # Create logging file
+    lib.setup_logger(
+        'debug', logfile='{0:s}run_ccal_plots.log'.format(output_path))
+    logger = logging.getLogger(__name__)
+    # logging.basicConfig(level=logging.DEBUG)
 
-end = timer()
-logger.info('Elapsed time to generate cross-calibration data QA inpection plots is {} minutes'.format(
-    (end - start)/60.))
-#time in minutes
+    # Create crosscal plots
+    crosscal_plots.make_all_ccal_plots(
+        args.scan, args.fluxcal, args.polcal, output_path=output_path)
+
+    end = timer()
+    logger.info('Elapsed time to generate cross-calibration data QA inpection plots is {} minutes'.format(
+        (end - start)/60.))
+    #time in minutes
