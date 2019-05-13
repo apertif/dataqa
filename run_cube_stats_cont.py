dc3a971fbabdcce7804db3a7c3ff3779ebba99c8
Robert Schulz <schulz@astron.nl>
2019-05-13 11:38:36 +0200

    Added trigger mode

--- run_cube_stats_cont.py
+++ run_cube_stats_cont.py
@@ -27,6 +27,10 @@ if __name__ == '__main__':
     parser.add_argument("obs_id", type=str,
                         help='Observation Number / Scan Number / TASK-ID')
 
+    # this mode will make the script look only for the beams processed by Apercal on a given node
+    parser.add_argument("--trigger_mode", action="store_true", default=False,
+                        help='Set to run for mosaic QA.')
+
     args = parser.parse_args()
 
     # get taskid/obs_id/scan
@@ -51,7 +55,11 @@ if __name__ == '__main__':
     host_name = socket.gethostname()
 
     # get data directories depending on the host name
-    if host_name != "happili-01":
+    if args.trigger_mode:
+        logger.info(
+            "--> Running line QA in trigger mode. Looking only for data processed by Apercal on {0:s} <--".format(host_name))
+        data_basedir_list = ['/data/apertif/{0:s}'.format(obs_id)]
+    if host_name != "happili-01" and not args.trigger_mode:
         logger.warning("You are not working on happili-01.")
         logger.warning("The script will not process all beams")
         logger.warning("Please switch to happili-01")
