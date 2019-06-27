27de6b136aab0474a1d1f992c243f1142b3c714d
Robert Schulz <schulz@astron.nl>
2019-06-27 19:16:11 +0200

    working on the image gallery

--- report/html_report_content_inspection_plots.py
+++ report/html_report_content_inspection_plots.py
@@ -10,7 +10,7 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-def write_obs_content_inspection_plots(html_code, qa_report_obs_path, page_type):
+def write_obs_content_inspection_plots(html_code, qa_report_obs_path, page_type, obs_info=None):
     """Function to create the html page for inspection plots
 
     Args:
@@ -26,53 +26,189 @@ def write_obs_content_inspection_plots(html_code, qa_report_obs_path, page_type)
 
     html_code += """
         <div class="w3-container w3-large">
-            <p>Here you can go through the different plots created by preflag.</p>
+            <p>Here you can go through the inspection plots.</p>
         </div>\n
         """
 
+    qa_report_obs_path_page = os.path.join(qa_report_obs_path, page_type)
+
     # Create html code for inspection plots
     # =====================================
-    # get images
-    image_list = glob.glob(
-        "{0:s}/{1:s}/*.png".format(qa_report_obs_path, page_type))
+    # assume that they are in the main directory if no obs infor are given
+    if obs_info is None:
 
-    if len(image_list) != 0:
+        # get images
+        image_list = glob.glob(os.path.join(qa_report_obs_path_page, "*.png"))
 
-        # sort the list
-        image_list.sort()
+        if len(image_list) != 0:
 
-        html_code += """
-                <div class="w3-container w3-margin-top">\n"""
-
-        img_counter = 0
-
-        for image in image_list:
-            if img_counter % 4 == 0:
-                html_code += """<div class="w3-row">\n"""
+            # sort the list
+            image_list.sort()
 
             html_code += """
-                <div class="w3-quarter">
-                    <a href="{0:s}/{1:s}">
-                        <img src="{0:s}/{1:s}" alt="No image" style="width:100%">
-                    </a>
-                    <!--<div class="w3-container w3-center">
-                        <h5>{1:s}</h5>
-                    </div>--!>
-                </div>\n""".format(page_type, os.path.basename(image))
+                    <div class="w3-container w3-margin-top">\n"""
 
-            if img_counter % 4 == 3 or img_counter == len(image_list)-1:
-                html_code += """</div>\n"""
+            img_counter = 0
 
-            img_counter += 1
+            for image in image_list:
+                if img_counter % 4 == 0:
+                    html_code += """<div class="w3-row">\n"""
 
-        html_code += """</div>\n"""
+                html_code += """
+                    <div class="w3-quarter">
+                        <a href="{0:s}/{1:s}">
+                            <img src="{0:s}/{1:s}" alt="No image" style="width:100%">
+                        </a>
+                        <!--<div class="w3-container w3-center">
+                            <h5>{1:s}</h5>
+                        </div>--!>
+                    </div>\n""".format(page_type, os.path.basename(image))
+
+                if img_counter % 4 == 3 or img_counter == len(image_list)-1:
+                    html_code += """</div>\n"""
+
+                img_counter += 1
+
+            html_code += """</div>\n"""
+        else:
+            logger.warning("No inspection plots found")
+            html_code += """
+            <div class="w3-container w3-large w3-text-red">
+                <p>
+                    No plots were found for preflag
+                </p>
+            </div>\n"""
+    # otherwise use the information
     else:
-        logger.warning("No inspection plots found")
-        html_code += """
-        <div class="w3-container w3-large w3-text-red">
-            <p>
-                No plots were found for preflag
-            </p>
-        </div>\n"""
+        if obs_info['Pol_Calibrator'][0] == '':
+            src_name = [obs_info['Target'][0], obs_info['Flux_Calibrator'][0]]
+        else:
+            src_name = [obs_info['Target'][0], obs_info['Flux_Calibrator']
+                        [0], obs_info['Pol_Calibrator'][0]]
+
+        # now go through the list of sources
+        for src in src_name:
+
+            qa_report_obs_path_page_src = os.path.join(
+                qa_report_obs_path_page, src)
+
+            button_src_name = "plot_{0:s}".format(src)
+
+            # for the target get the images that are in the directory
+            if src == obs_info['Target'][0]:
+
+                image_list = glob.glob(os.path.join(
+                    qa_report_obs_path_page_src, "*.png"))
+
+                # check that there are actually images
+                if len(image_list) != 0:
+
+                    # sort the list
+                    image_list.sort()
+
+                    # create container for gallery
+                    html_code += """
+                           <div class="w3-container">
+                                <button class="w3-btn w3-large w3-center w3-block w3-border-gray w3-amber w3-hover-yellow w3-margin-bottom" onclick="show_hide_plots('{0:s}')">
+                                {1:s}
+                                </button>
+                            </div>
+                            <div class="w3-container w3-margin-top">\n""".format(button_src_name, src)
+
+                    # to count the plots for the gallery
+                    img_counter = 0
+
+                    # go through the images
+                    for image in image_list:
+
+                        # use 4 plots per row
+                        if img_counter % 4 == 0:
+                            html_code += """<div class="w3-row">\n"""
+
+                        # no caption need for the image
+                        html_code += """
+                            <div class="w3-quarter">
+                                <a href="{0:s}/{1:s}/{2:s}">
+                                    <img src="{0:s}/{1:s}/{2:s}" alt="No image" style="width:100%">
+                                </a>
+                            </div>\n""".format(page_type, src, os.path.basename(image))
+
+                        # to close a row after four images or when there are no more images
+                        if img_counter % 4 == 3 or img_counter == len(image_list)-1:
+                            html_code += """</div>\n"""
+
+                        img_counter += 1
+
+                    # close the gallery div
+                    html_code += """</div>\n"""
+
+                # otherwise create a disabled button
+                else:
+                    logger.warning("No inspection plots found for target {0}".format(
+                        src))
+                    html_code += """
+                    <div class="w3-container">
+                        <button class="w3-btn w3-large w3-center w3-block w3-border-gray w3-amber w3-hover-yellow w3-margin-bottom w3-disabled" class="disabled" onclick="show_hide_plots('{0:s}')">
+                            {1:s}
+                        </button>
+                    </div>\n""".format(button_src_name, src)
+
+            # for the calibrator, the plots in beam directories
+            else:
+
+                # get a list of beams instead of getting all plots
+                # because they will be separated in polarization
+                beam_list = np.array(glob.glob(os.path.join(
+                    qa_report_obs_path_page_src, "[0-3][0-9]")))
+
+                # check that there are images
+                if len(beam_list) != 0:
+
+                    # an array of just the beams
+                    beam_nr_list = np.array(
+                        [os.path.basename(beam) for beam in beam_list])
+
+                    # a reference list of beams
+                    beam_ref_list = np.array(
+                        ['{0:02d}'.format(beam) for beam in range(40)])
+
+                    # the list of polarisation of the plots
+                    pol_list = ['xx', 'yy']
+
+                    html_code += """
+                            <div class="w3-container">
+                                <button class="w3-btn w3-large w3-center w3-block w3-border-gray w3-amber w3-hover-yellow w3-margin-bottom" onclick="show_hide_plots('{0:s}')">
+                                {1:s}
+                                </button>
+                            </div>
+                            <div class="w3-container w3-margin-top w3-hide" name="{0:s}">\n""".format(button_src_name, src)
+
+                    # now go through each of the polarisations
+                    for pol in pol_list:
+                        div_name_pol = "insplot_gallery_{0:s}".format(pol)
+
+                        # get a list of images
+
+                        html_code += """
+                                    <div class="w3-container">
+                                        <button class="w3-btn w3-large w3-center w3-block w3-border-gray w3-dark-gray w3-hover-gray w3-margin-bottom" class="button_continuum" onclick="show_hide_plots('{0:s}')">
+                                            {1:s}
+                                        </button>
+                                    </div>
+                                    <div class="w3-container w3-margin-top w3-hide" name="{0:s}">\n""".format(div_name_pol, pol)
+
+                    html_code += """
+                            </div>\n"""
+
+                # otherwise create a disabled button
+                else:
+                    logger.warning("No inspection plots found for calibrator {0}".format(
+                        src))
+                    html_code += """
+                    <div class = "w3-container" >
+                        <button class = "w3-btn w3-large w3-center w3-block w3-border-gray w3-amber w3-hover-yellow w3-margin-bottom w3-disabled" class = "disabled" onclick = "show_hide_plots('{0:s}')" >
+                            {1: s}
+                        </button >
+                    </div >\n""".format(button_src_name, src)
 
     return html_code
