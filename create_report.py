1cb3ab5da1fcc0993c67467c97a28f5656c9724f
Robert Schulz <schulz@astron.nl>
2019-07-19 15:09:40 +0200

    reading timing requires additional argument now, not run by default

--- create_report.py
+++ create_report.py
@@ -68,7 +68,7 @@ def main():
     parser.add_argument("-c", "--combine", action="store_true", default=False,
                         help='(Depracated) Set to create a combined report from all happilis on happili-01. It will overwrite the report on happili-01')
 
-    parser.add_argument("--do_not_read_timing", action="store_true", default=False,
+    parser.add_argument("--read_timing", action="store_true", default=False,
                         help='Set to avoid reading timing information. Makes only sense if script is run multiple times or for debugging')
 
     # this mode will make the script look only for the beams processed by Apercal on a given node
@@ -181,7 +181,7 @@ def main():
     #                     format='%(asctime)s - %(levelname)s: %(message)s')
 
     # getting timing measurment for apercal
-    if not add_osa_report and not args.do_not_read_timing:
+    if not add_osa_report and args.read_timing:
         try:
             get_pipeline_run_time(obs_id, trigger_mode=args.trigger_mode)
         except Exception as e:
