8e03790b4a5c9d3c3ad891a2cbafa3fedcc3cef4
Robert Schulz <schulz@astron.nl>
2019-07-24 12:31:42 +0200

    function for dish delay plot

---
+++ crosscal/dish_delay_plot.py
@@ -0,0 +1,14 @@
+"""
+Simple function to plot the dish delay.
+This needs to run separately from the other crosscal plots because
+it requires all 40 beams to be accessible
+"""
+
+from crosscal_plots import GDSols
+
+
+def get_dish_delay_plots(obs_id, fluxcal):
+
+    GD = GDSols(obs_id, fluxcal, False)
+    GD.get_data()
+    GD.plot_dish_delay()
