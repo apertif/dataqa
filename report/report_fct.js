821e8570b5d5057fe6be47011545c8c8e9740cd9
Robert Schulz <schulz@astron.nl>
2019-07-25 08:53:47 +0200

    Added function for beamweights slide show

--- report/report_fct.js
+++ report/report_fct.js
@@ -2,6 +2,51 @@
 JavaScript functions for the science commissioning report
 */
 
+// starting value for the slide index for the beamweights
+// since there are 40 beams, we need 40 different slideshows
+var slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+function change_slide(n, gallery_name, label_name) {
+    /*
+    This function controls the galleries of the beamweights page.
+
+    It allows to click through the images forwards and backwards. 
+    It also changes the Subband value in the caption
+    */
+    //console.log(gallery_name)
+    var i;
+    // get all the elements for this gallery
+    var x = document.getElementsByName(gallery_name);
+    //extract the index of the slideshow from the name of the element
+    slideshow_index = Number(gallery_name.replace('slideshow', ''))
+    slideIndex[slideshow_index] += n;
+
+    //console.log(slideshow_index);
+    // if the slide counter is larger then the number of images, start at the beginning
+    if (slideIndex[slideshow_index] > x.length) { slideIndex[slideshow_index] = 1; }
+    // if the slide counter is smaller than one, go to last image
+    if (slideIndex[slideshow_index] < 1) { slideIndex[slideshow_index] = x.length; }
+
+    // hide all elements
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace("w3-show", " w3-hide");
+        //x[i].style.display = "none";
+    }
+    // and show only the one corresponding to the slide counter
+    x[slideIndex[slideshow_index] - 1].className = x[slideIndex[slideshow_index] - 1].className.replace("w3-hide", " w3-show");
+
+    // Change the subband value in the caption
+    // get the subband value
+    var src_name_array = x[slideIndex[slideshow_index] - 1].src.split("/")
+    var image_name = src_name_array[src_name_array.length - 1]
+    var subband_value = image_name.split("_")[3].replace('S', '')
+    // change the label (there is only one)
+    label = document.getElementsByName(label_name);
+    var old_subband_value = label[0].innerHTML.slice(-3)
+    //console.log(label[0].innerHTML)
+    label[0].innerHTML = label[0].innerHTML.replace(old_subband_value, subband_value)
+}
+
 function show_hide_plots(gallery_name) {
     /*
     This function enables show and hide capabilities for the plots.
