f1180a77483f7ddbbb7c23f25def0acc7bbe5d6a
Robert Schulz <schulz@astron.nl>
2020-01-08 11:10:43 +0100

    added main statement

--- run_ccal_plots.py
+++ run_ccal_plots.py
@@ -13,51 +13,52 @@ import logging
 import os
 from apercal.libs import lib
 
-start = timer()
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser(description='Generate crosscal QA plots')
+    start = timer()
 
-# 1st argument: File name
-parser.add_argument("scan", help='Scan of target field')
-parser.add_argument("fluxcal", help='Fluxcal name')
-parser.add_argument("polcal", help='Polcal name')
+    parser = argparse.ArgumentParser(description='Generate crosscal QA plots')
 
-parser.add_argument('-p', '--path', default=None,
-                    help='Destination for images')
+    # 1st argument: File name
+    parser.add_argument("scan", help='Scan of target field')
+    parser.add_argument("fluxcal", help='Fluxcal name')
+    parser.add_argument("polcal", help='Polcal name')
 
-parser.add_argument('-b', '--basedir', default=None,
-                    help='Data directory')
+    parser.add_argument('-p', '--path', default=None,
+                        help='Destination for images')
 
+    parser.add_argument('-b', '--basedir', default=None,
+                        help='Data directory')
 
-# this mode will make the script look only for the beams processed by Apercal on a given node
-parser.add_argument("--trigger_mode", action="store_true", default=False,
-                    help='Set it to run Autocal triggering mode automatically after Apercal.')
+    # this mode will make the script look only for the beams processed by Apercal on a given node
+    parser.add_argument("--trigger_mode", action="store_true", default=False,
+                        help='Set it to run Autocal triggering mode automatically after Apercal.')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# If no path is given change to default QA path
-if args.path is None:
-    output_path = get_default_imagepath(args.scan, basedir=args.basedir)
+    # If no path is given change to default QA path
+    if args.path is None:
+        output_path = get_default_imagepath(args.scan, basedir=args.basedir)
 
-    # check that crosscal qa directory exists
-    output_path = "{0:s}crosscal/".format(output_path)
+        # check that crosscal qa directory exists
+        output_path = "{0:s}crosscal/".format(output_path)
 
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-else:
-    output_path = args.path
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+    else:
+        output_path = args.path
 
-# Create logging file
-lib.setup_logger(
-    'debug', logfile='{0:s}run_ccal_plots.log'.format(output_path))
-logger = logging.getLogger(__name__)
-# logging.basicConfig(level=logging.DEBUG)
+    # Create logging file
+    lib.setup_logger(
+        'debug', logfile='{0:s}run_ccal_plots.log'.format(output_path))
+    logger = logging.getLogger(__name__)
+    # logging.basicConfig(level=logging.DEBUG)
 
-# Create crosscal plots
-crosscal_plots.make_all_ccal_plots(
-    args.scan, args.fluxcal, args.polcal, output_path=output_path, basedir=args.basedir, trigger_mode=args.trigger_mode)
+    # Create crosscal plots
+    crosscal_plots.make_all_ccal_plots(
+        args.scan, args.fluxcal, args.polcal, output_path=output_path, basedir=args.basedir, trigger_mode=args.trigger_mode)
 
-end = timer()
-logger.info('Elapsed time to generate cross-calibration data QA inpection plots is {} minutes'.format(
-    (end - start)/60.))
-#time in minutes
+    end = timer()
+    logger.info('Elapsed time to generate cross-calibration data QA inpection plots is {} minutes'.format(
+        (end - start)/60.))
+    #time in minutes
--- run_scal_plots.py
+++ run_scal_plots.py
@@ -14,99 +14,100 @@ import time
 from apercal.libs import lib
 import logging
 
-start = timer()
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser(description='Generate selfcal QA plots')
+    start = timer()
 
-# 1st argument: File name
-parser.add_argument("scan", help='Scan of target field')
-parser.add_argument("target", help='Target name')
+    parser = argparse.ArgumentParser(description='Generate selfcal QA plots')
 
-parser.add_argument('-p', '--path', default=None,
-                    help='Destination for images')
+    # 1st argument: File name
+    parser.add_argument("scan", help='Scan of target field')
+    parser.add_argument("target", help='Target name')
 
-parser.add_argument('-b', '--basedir', default=None,
-                    help='Data directory')
+    parser.add_argument('-p', '--path', default=None,
+                        help='Destination for images')
 
-parser.add_argument('-M', '--maps', default=True,
-                    action='store_false', help='Do not generate selfcal maps')
-parser.add_argument('-P', '--phase', default=True,
-                    action='store_false', help='Do not generate phase plots')
-parser.add_argument('-A', '--amplitude', default=True,
-                    action='store_false', help='Do not generate amplitude plots')
+    parser.add_argument('-b', '--basedir', default=None,
+                        help='Data directory')
 
-# this mode will make the script look only for the beams processed by Apercal on a given node
-parser.add_argument("--trigger_mode", action="store_true", default=False,
-                    help='Set it to run Autocal triggering mode automatically after Apercal.')
+    parser.add_argument('-M', '--maps', default=True,
+                        action='store_false', help='Do not generate selfcal maps')
+    parser.add_argument('-P', '--phase', default=True,
+                        action='store_false', help='Do not generate phase plots')
+    parser.add_argument('-A', '--amplitude', default=True,
+                        action='store_false', help='Do not generate amplitude plots')
 
-args = parser.parse_args()
+    # this mode will make the script look only for the beams processed by Apercal on a given node
+    parser.add_argument("--trigger_mode", action="store_true", default=False,
+                        help='Set it to run Autocal triggering mode automatically after Apercal.')
 
-# If no path is given change to default QA path
-if args.path is None:
-    output_path = get_default_imagepath(args.scan, basedir=args.basedir)
+    args = parser.parse_args()
 
-    # check that selfcal qa directory exists
-    output_path = os.path.join(output_path, "selfcal/")
+    # If no path is given change to default QA path
+    if args.path is None:
+        output_path = get_default_imagepath(args.scan, basedir=args.basedir)
 
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-else:
-    output_path = args.path
+        # check that selfcal qa directory exists
+        output_path = os.path.join(output_path, "selfcal/")
 
-# Create log file
-lib.setup_logger(
-    'info', logfile='{0:s}run_scal_plots.log'.format(output_path))
-logger = logging.getLogger(__name__)
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+    else:
+        output_path = args.path
 
-# Get selfcal maps
-if args.maps:
-    try:
-        logger.info("#### Creating selfcal maps ...")
-        start_time_maps = time.time()
-        get_selfcal_maps(args.scan, output_path,
-                         trigger_mode=args.trigger_mode)
-        logger.info("#### Creating selfcal maps. Done ({0:.0f}s)".format(
-            time.time()-start_time_maps))
-    except Exception as e:
-        logger.error(e)
-        logger.error("#### Creating selfcal maps failed")
-else:
-    logger.info("#### Not generating selfcal maps")
+    # Create log file
+    lib.setup_logger(
+        'info', logfile='{0:s}run_scal_plots.log'.format(output_path))
+    logger = logging.getLogger(__name__)
 
-# Get phase plots
-if args.phase:
-    try:
-        logger.info("#### Creating phase plots")
-        start_time_plots = time.time()
-        PH = scplots.PHSols(args.scan, args.target,
-                            trigger_mode=args.trigger_mode, basedir=args.basedir)
-        PH.get_data()
-        PH.plot_phase(imagepath=output_path)
-        logger.info('#### Done with phase plots ({0:.0f}s)'.format(
-            time.time()-start_time_plots))
-    except Exception as e:
-        logger.error(e)
-        logger.error("Creating phase plots failed.")
-else:
-    logger.info("#### Not generating phase plots")
+    # Get selfcal maps
+    if args.maps:
+        try:
+            logger.info("#### Creating selfcal maps ...")
+            start_time_maps = time.time()
+            get_selfcal_maps(args.scan, output_path,
+                             trigger_mode=args.trigger_mode)
+            logger.info("#### Creating selfcal maps. Done ({0:.0f}s)".format(
+                time.time()-start_time_maps))
+        except Exception as e:
+            logger.error(e)
+            logger.error("#### Creating selfcal maps failed")
+    else:
+        logger.info("#### Not generating selfcal maps")
 
-# Get amplitude plots
-if args.amplitude:
-    try:
-        logger.info("#### Creating amplitude plots")
-        start_time_plots = time.time()
-        AMP = scplots.AMPSols(args.scan, args.target,
-                              trigger_mode=args.trigger_mode, basedir=args.basedir)
-        AMP.get_data()
-        AMP.plot_amp(imagepath=output_path)
-        logger.info('#### Done with amplitude plots ({0:.0f}s)'.format(
-            time.time()-start_time_plots))
-    except Exception as e:
-        logger.error(e)
-        logger.error("Creating amplitude plots failed.")
-else:
-    logger.info("#### Not generating amplitude plots")
+    # Get phase plots
+    if args.phase:
+        try:
+            logger.info("#### Creating phase plots")
+            start_time_plots = time.time()
+            PH = scplots.PHSols(args.scan, args.target,
+                                trigger_mode=args.trigger_mode, basedir=args.basedir)
+            PH.get_data()
+            PH.plot_phase(imagepath=output_path)
+            logger.info('#### Done with phase plots ({0:.0f}s)'.format(
+                time.time()-start_time_plots))
+        except Exception as e:
+            logger.error(e)
+            logger.error("Creating phase plots failed.")
+    else:
+        logger.info("#### Not generating phase plots")
 
+    # Get amplitude plots
+    if args.amplitude:
+        try:
+            logger.info("#### Creating amplitude plots")
+            start_time_plots = time.time()
+            AMP = scplots.AMPSols(args.scan, args.target,
+                                  trigger_mode=args.trigger_mode, basedir=args.basedir)
+            AMP.get_data()
+            AMP.plot_amp(imagepath=output_path)
+            logger.info('#### Done with amplitude plots ({0:.0f}s)'.format(
+                time.time()-start_time_plots))
+        except Exception as e:
+            logger.error(e)
+            logger.error("Creating amplitude plots failed.")
+    else:
+        logger.info("#### Not generating amplitude plots")
 
-end = timer()
-print 'Elapsed time to generate self-calibration data QA inpection plots and images is {} minutes'.format((end - start)/60.)
+    end = timer()
+    print 'Elapsed time to generate self-calibration data QA inpection plots and images is {} minutes'.format((end - start)/60.)
